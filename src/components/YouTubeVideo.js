87bb575c15fd28758cf4ed5458d8a208a3aaa6d3
Olamide Kolawole <>
2018-10-13 07:50:01 -0500

    add margin to youtube video on homepage

--- src/components/YouTubeVideo.js
+++ src/components/YouTubeVideo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function YoutubeVideo(props) {
   return (
-    <div className="embed-responsive embed-responsive-16by9">
+    <div style={{"margin-left":"30px"}} className="embed-responsive embed-responsive-16by9">
       <iframe
         src={props.url}
         title={props.url}
